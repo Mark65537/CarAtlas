9b721b3ef2c08881143e452ddfcb509f013a7e1c
mark65537 <grigorash-mark@mail.ru>
2025-11-19 09:07:47 +0700

    запуск сервера

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,26 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
+import { gql } from "apollo-server";
+
+// Минимальная схема для запуска сервера
+const typeDefs = gql`
+  type Query {
+    _empty: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    _empty: () => "Apollo Server запущен!"
+  }
+};
 
 async function start() {
-  // опционально seed
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  
   const { url } = await server.listen({ port: 4000 });
   console.log(`Server ready at ${url}`);
 }
